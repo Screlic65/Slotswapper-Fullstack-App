6f77b8248b82f2d5c22a582b1a71a5bf4a48375b
Darshan Momaya <darshanmomaya65@gmail.com>
2025-11-07 23:37:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+
 const http = require('http');
 const { Server } = require("socket.io");
 const { initSocket, attachSocket } = require('./middleware/socketMiddleware');
